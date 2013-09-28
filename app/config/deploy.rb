d98d82e84a065713ff578e37bdbc7403f64a6929
Camilo Cota <camilocot@gmail.com>
2013-09-28 13:29:36 +0200

    Nuevos valores para el deploy

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -17,9 +17,24 @@ set  :use_sudo,       false
 set  :keep_releases,  3
 set  :use_composer,   true
 set  :update_vendors, true
-set  :shared_files,   ["app/config/parameters.yml"]
-set  :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor"]
+set  :shared_files,        ["app/config/parameters.yml","web/js/fos_js_routes.js"]
+set  :shared_children,     [app_path + "/logs", web_path + "/uploads", "vendor" , app_path + "/logs"]
 set  :dump_assetic_assets, true
 
 # Be more verbose by uncommenting the following line
 logger.level = Logger::MAX_LEVEL
+
+# Symfony2 2.1
+before 'symfony:composer:update', 'symfony:copy_vendors'
+
+namespace :symfony do
+  desc "Copy vendors from previous release"
+  task :copy_vendors, :except => { :no_release => true } do
+    if Capistrano::CLI.ui.agree("Do you want to copy last release vendor dir then do composer install ?: (y/N)")
+      capifony_pretty_print "--> Copying vendors from previous release"
+
+      run "cp -a #{previous_release}/vendor #{latest_release}/"
+      capifony_puts_ok
+    end
+  end
+end
