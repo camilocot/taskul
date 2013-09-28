5ee666d5be367ac9e2ff94cf3389fd16ed689595
Camilo Cota <camilocot@gmail.com>
2013-09-28 10:56:38 +0200

    Capifony Deploy

---
+++ app/config/deploy.rb
@@ -0,0 +1,23 @@
+set :application, "taskul"
+set :domain,      "#{application}.net"
+set :deploy_to,   "/var/www/#{domain}"
+set :app_path,    "app"
+
+set :repository,  "git@bitbucket.org:camilocot/#{application}.git"
+set :scm,         :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+
+set :model_manager, "doctrine"
+# Or: `propel`
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+
+set  :use_sudo,       false
+set  :keep_releases,  3
+set  :use_composer,   true
+set  :update_vendors, true
+set  :shared_files,   ["app/config/parameters.yml"]
+
+# Be more verbose by uncommenting the following line
+# logger.level = Logger::MAX_LEVEL
