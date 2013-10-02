375effc74af000966c6a8a4c6a071854368c1270
Camilo Cota <camilocot@gmail.com>
2013-10-02 20:41:46 +0200

    Añadido bbcode a los mensajes

--- src/Taskul/MessageBundle/Resources/public/js/newThread.js
+++ src/Taskul/MessageBundle/Resources/public/js/newThread.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-	new nicEditor({fullPanel : true}).panelInstance('message_body');
-
-    $('button[type=submit]').click(function(event){
-        event.preventDefault();
-        ne = nicEditors.findEditor('message_body');
-        $('#message_body').val(ne.getContent());
-        $('#form_thread').submit();
-    });
 
     if ($.fn.select2) {
         $("select#message_recipient").select2();
